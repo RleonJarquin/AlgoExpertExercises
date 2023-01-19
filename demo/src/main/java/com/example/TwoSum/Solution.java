f9d6a2f6a3bd20f88c228b84ca3de8d2454a425c
Ruben Leon <98782056+RleonJarquin@users.noreply.github.com>
2023-01-19 14:49:00 -0800

    Created Solution + Initialized problem
    Plenty of errors in regard to syntax and Other stuff.

---
+++ demo/src/main/java/com/example/TwoSum/Solution.java
@@ -0,0 +1,25 @@
+package com.example.TwoSum;
+
+import java.util.*;
+
+public class Solution {
+
+    // Given an array of integers nums and an integer target, return indices of the
+    // two numbers such that they add up to target.
+
+    // You may assume that each input would have exactly one solution, and you may
+    // not use the same element twice.
+
+    int nums[] = { 2, 7, 11, 15 };
+
+    int targetNum = 9;
+
+    HashMap<Integer, Integer> twoSumSearch = new HashMap<>();
+
+    for(
+    int x = 0;nums.length>x;x++)
+    {
+
+    }
+
+}
