65e1297e23474b4dd424c9b55507ac62829686f7
Ruben Leon <98782056+RleonJarquin@users.noreply.github.com>
2023-01-20 11:45:06 -0800

    Eliminated test file and created the function for SubSequence function
    Moved everything into one file.

--- demo/src/main/java/com/ValidateSubSequence/Program.java
+++ demo/src/main/java/com/ValidateSubSequence/Program.java
@@ -3,29 +3,32 @@ package com.ValidateSubSequence;
 import java.util.*;
 
 class Program {
-  public static boolean isValidSubsequence(List<Integer> array, List<Integer> sequence) {
-    // Write your code here. 
-    int seqIdx = 0;
+    public static boolean isValidSubsequence(List<Integer> array, List<Integer> sequence) {
+        // Write your code here.
+        int seqIdx = 0;
 
-    // Loop through the array and if the value is in the array traverse through the sequence by one
-    for(int x = 0; x < array.size(); x++){
-        if(array.get(x) == sequence.get(seqIdx)){
-            x++;
-        }
-        if(seqIdx ==  sequence.size()){
-            return true;
+        // Loop through the array and if the value is in the array traverse through the
+        // sequence by one
+        for (int x = 0; x < array.size(); x++) {
+            if (array.get(x) == sequence.get(seqIdx)) {
+                seqIdx++;
+            }
+            if (seqIdx == sequence.size()) {
+                return true;
+            }
+
         }
 
+        return false;
+    }
+
+    public static void main(String[] args) {
+
+        List<Integer> array = Arrays.asList(5, 1, 22, 25, 6, -1, 8, 10);
+        List<Integer> sequence = Arrays.asList(1, 6, -1, 10);
+
+        System.out.println(isValidSubsequence(array, sequence));
 
     }
 
-    
-    return false;
-  }
 }
-
-
- 
-    
-
-
