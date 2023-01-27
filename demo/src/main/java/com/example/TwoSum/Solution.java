1cb4de257df4987cc6ec9de93341c871083e9ceb
Ruben Leon <98782056+RleonJarquin@users.noreply.github.com>
2023-01-27 13:45:23 -0800

    TwoSum Hashmap logic created
    Made the key  y = 10 - x and the value x. If the y value is in the key set then return the array otherwise put the value in the hashmap.

--- demo/src/main/java/com/example/TwoSum/Solution.java
+++ demo/src/main/java/com/example/TwoSum/Solution.java
@@ -10,15 +10,30 @@ public class Solution {
     // You may assume that each input would have exactly one solution, and you may
     // not use the same element twice.
 
+    // Loop through the array
+
     public static int[] twoSumSearch(int[] array, int targetSum) {
 
         HashMap<Integer, Integer> twoSumSearch = new HashMap<>();
 
+        // Check if the y is in the hashmap otherwise put in the hashmap.
+        // If it is return the array with both values
+
+        int[] twoSumArray;
+
         for (int x = 0; array.length > x; x++) {
+            int y = 10 - array[x];
+            if (twoSumSearch.keySet().contains(y)) {
+                twoSumArray = new int[] { array[x], y };
+                return twoSumArray;
+            }
+            else{
+                twoSumSearch.put(y,array[x]);
+            }
 
         }
 
-        return null;
+        return new int [] {};
 
     }
 
@@ -26,6 +41,8 @@ public class Solution {
         int array[] = { 2, 7, 11, 15 };
 
         int targetNum = 9;
+
+        
 
     }
 
